6d7d473f6f6ad7311f2ca7dca675fc8f1b9bf554
X Maze <71417489+ketchupmaze@users.noreply.github.com>
2023-11-07 01:38:48 +0700

    Update and rename _antilinknokick.js to _antilinkdelete.js

--- plugins/_antilinkdelete.js
+++ plugins/_antilinkdelete.js
@@ -1,21 +1,3 @@
-/*let handler = m => m
-
-let linkRegex = /(push|.push)/i
-handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
-  if ((m.isBaileys && m.fromMe) || m.fromMe || !m.isGroup) return true
-  let chat = global.db.data.chats[m.chat]
-  let isGroupLink = linkRegex.exec(m.text)
-  if (chat.pushKontak && isGroupLink) {
-    if (isAdmin) return
-    if (!isBotAdmin) return m.reply('*YuLa is not admin!!!, how does YuLa kick people?😠*')
-    await conn.sendMessage(m.chat, { delete: m.key })
-    await conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
-  }
-  return true
-}
-
-module.exports = handler*/
-
 let handler = m => m
 
 let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
