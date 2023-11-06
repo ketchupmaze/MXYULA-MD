72cfd4306813c125fa799091480d3b49ed0be1c7
X Maze <71417489+ketchupmaze@users.noreply.github.com>
2023-11-07 01:17:40 +0700

    Update Ai-txt2img.js

--- plugins/Ai-txt2img.js
+++ plugins/Ai-txt2img.js
@@ -6,15 +6,15 @@ const handler = async (m, { conn, args, text, usedPrefix, command }) => {
 *example:* ${usedPrefix}${command} hijab, beautiful, jumpsuit, face korean, Weight 60kg, 20 year, Cannon, 8K, HDR, eyes same,
 `
   if (!text.includes(',')) throw `Tolong gunakan prompt dengan benar. Gunakan koma *[ , ]* untuk memisahkan argumen.\n*Contoh:* ${usedPrefix}${command} 1girl, blush, looking to viewer, warm smile`;  
-  m.reply(`_Preparing ${command} diffusion..._`)
+  m.reply(`_Preparing ${command}..._`)
   const payload = {
-    server_name: "rose",
+    server_name: "frieren",
     prompt: `${text}`,
     negative_prompt: "missing fingers,missing arms,missing legs,extra digit, extra arms, extra leg, extra foot, sketches, (worst quality:2), (low quality:2), (normal quality:2), lowres, normal quality, ((monochrome)), ((grayscale)), skin spots, acnes, skin blemishes, bad anatomy,(fat:1.2),facing away, looking away,tilted head, lowres,bad anatomy,bad hands, text, error, missing fingers,extra digit, fewer digits, cropped, worstquality, low quality, normal quality,jpegartifacts,signature, watermark, username,blurry,bad feet,cropped,poorly drawn hands,poorly drawn face,mutation,deformed,worst quality,low quality,normal quality,jpeg artifacts,signature,watermark,extra fingers,fewer digits,extra limbs,extra arms,extra legs,malformed limbs,fused fingers,too many fingers,long neck,cross-eyed,mutated hands,polar lowres,bad body,bad proportions,gross proportions,text,error,",
     width: 512,
     height: 768,
     steps: 25,
-    model_id: "epicrealism-v4",
+    model_id: "hyperrealism",
     sampler: "UniPC",
     cfg: 7,
     seed: null,
@@ -24,7 +24,7 @@ const handler = async (m, { conn, args, text, usedPrefix, command }) => {
     panorama: "no",
     safety_checker: "no",
     safety_checker_type: "blur",
-    lora_model: "more_details",
+    lora_model: "add_detail",
     lora_strength: 1,
     clip_skip: 2,
     embeddings_model: "",
@@ -61,7 +61,7 @@ const handler = async (m, { conn, args, text, usedPrefix, command }) => {
 *guidance_scale*: ${cfg}
 *steps*: ${step}
 *seed*: ${seed}`
- 
+conn.reply(m.chat, medata, m)
     for (const image of images) {
       await new Promise((resolve) => {
         setTimeout(async () => {
@@ -70,14 +70,13 @@ const handler = async (m, { conn, args, text, usedPrefix, command }) => {
         }, generation_time * 1000);
       });
     }
-  m.reply(medata)
   }
 };
 
 handler.help = ['text2image', 'diffusion'];
 handler.tags = ['ai'];
-handler.command =  /^(diff|diffusion|text2image|txt2img|t2i)$/i
-handler.limit = 5;
+handler.command =  /^(diff|diffusion|text2image|txt2img|text2img|dif)$/i
+handler.limit = true;
 handler.register = false;
 handler.premium = false;
 
