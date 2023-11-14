337c0c3c1c7c068b528d40a916b165aa9ce81326
X Maze <71417489+ketchupmaze@users.noreply.github.com>
2023-11-14 11:20:04 +0700

    Update nsfw-random.js

--- plugins/nsfw-random.js
+++ plugins/nsfw-random.js
@@ -1,151 +1,150 @@
 const fetch = require('node-fetch');
-const fs = require('fs');
 
 const handler = async (m, { conn, command, usedPrefix }) => {
   if (m.isGroup && !db.data.chats[m.chat].nsfw) throw `Fitur *NSFW* BELUM AKTIF di grup ini\n\nAktifkan Fitur ini dengan menulis *.enable nsfw*`;
   let text2 = `SANGEAN`;
 
- /* if (command == 'lolinsfw' ) {
-    const blow = JSON.parse(fs.readFileSync('./json/nsfwloli.json'));
-    const res = blow[Math.floor(Math.random() * blow.length)];
-    conn.sendFile(m.chat, res, 'foto.jpg', text2, m);
-  }*/
-
   if (command == 'ahegao' ) {
-    const ass = JSON.parse(fs.readFileSync('./json/ahegao.json'));
+    const ass = await (await fetch(`https://raw.githubusercontent.com/ketchupmaze/AssistenYulaDB/main/nsfw/ahegao.json`)).json()
     const res = ass[Math.floor(Math.random() * ass.length)];
     conn.sendFile(m.chat, res, 'foto.jpg', text2, m);
   }
 
   if (command == 'ass' ) { 
-    const yuri = JSON.parse(fs.readFileSync('./json/ass.json'));
+    const yuri = await (await fetch(`https://raw.githubusercontent.com/ketchupmaze/AssistenYulaDB/main/nsfw/yuri.json`)).json()
     const res = yuri[Math.floor(Math.random() * yuri.length)];
     conn.sendFile(m.chat, res, 'foto.jpg', text2, m);
   }
 
   if (command == 'bdsm' ) { 
-    const bdsm = JSON.parse(fs.readFileSync('./json/bdsm.json'));
+    const bdsm = await (await fetch(`https://raw.githubusercontent.com/ketchupmaze/AssistenYulaDB/main/nsfw/bdsm.json`)).json()
     const res = bdsm[Math.floor(Math.random() * bdsm.length)];
     conn.sendFile(m.chat, res, 'foto.jpg', text2, m);
   }
 
   if (command == 'blowjob' ) { 
-    const cum = JSON.parse(fs.readFileSync('./json/blowjob.json'));
+    const cum = await (await fetch(`https://raw.githubusercontent.com/ketchupmaze/AssistenYulaDB/main/nsfw/blowjob.json`)).json()
     const res = cum[Math.floor(Math.random() * cum.length)];
     conn.sendFile(m.chat, res, 'foto.jpg', text2, m);
   }
 
   if (command == 'cuckold' ) { 
-    const ero = JSON.parse(fs.readFileSync('./json/cuckold.json'));
+    const ero = await (await fetch(`https://raw.githubusercontent.com/ketchupmaze/AssistenYulaDB/main/nsfw/cuckold.json`)).json()
     const res = ero[Math.floor(Math.random() * ero.length)];
     conn.sendFile(m.chat, res, 'foto.jpg', text2, m);
   }
 
   if (command == 'cum' ) { 
-    const foot = JSON.parse(fs.readFileSync('./json/cum.json'));
+    const foot = await (await fetch(`https://raw.githubusercontent.com/ketchupmaze/AssistenYulaDB/main/nsfw/cum.json`)).json()
     const res = foot[Math.floor(Math.random() * foot.length)];
+    conn.sendFile(m.chat, res, 'foto.jpg', text2, m);
+  }
+  
+  if (command == 'eba' ) { 
+    const glasses = await (await fetch(`https://raw.githubusercontent.com/ketchupmaze/AssistenYulaDB/main/nsfw/eba.json`)).json()
+    const res = glasses[Math.floor(Math.random() * glasses.length)];
     conn.sendFile(m.chat, res, 'foto.jpg', text2, m);
   }
 
   if (command == 'ero' ) { 
-    const glasses = JSON.parse(fs.readFileSync('./json/ero.json'));
+    const glasses = await (await fetch(`https://raw.githubusercontent.com/ketchupmaze/AssistenYulaDB/main/nsfw/ero.json`)).json()
     const res = glasses[Math.floor(Math.random() * glasses.length)];
     conn.sendFile(m.chat, res, 'foto.jpg', text2, m);
   }
 
   if (command == 'femdom' ) { 
-    const jahy = JSON.parse(fs.readFileSync('./json/femdom.json'));
+    const jahy = await (await fetch(`https://raw.githubusercontent.com/ketchupmaze/AssistenYulaDB/main/nsfw/femdom.json`)).json()
     const res = jahy[Math.floor(Math.random() * jahy.length)];
     conn.sendFile(m.chat, res, 'foto.jpg', text2, m);
   }
 
   if (command == 'foot' ) { 
-    const orgy = JSON.parse(fs.readFileSync('./json/foot.json'));
+    const orgy = await (await fetch(`https://raw.githubusercontent.com/ketchupmaze/AssistenYulaDB/main/nsfw/foot.json`)).json()
     const res = orgy[Math.floor(Math.random() * orgy.length)];
     conn.sendFile(m.chat, res, 'foto.jpg', text2, m);
   }
 
   if (command == 'gangbang' ) { 
-    const orgy = JSON.parse(fs.readFileSync('./json/gangbang.json'));
+    const orgy = await (await fetch(`https://raw.githubusercontent.com/ketchupmaze/AssistenYulaDB/main/nsfw/gangbang.json`)).json()
     const res = orgy[Math.floor(Math.random() * orgy.length)];
     conn.sendFile(m.chat, res, 'foto.jpg', text2, m);
   }
 
   if (command == 'glasses' ) { 
-    const orgy = JSON.parse(fs.readFileSync('./json/glasses.json'));
+    const orgy = await (await fetch(`https://raw.githubusercontent.com/ketchupmaze/AssistenYulaDB/main/nsfw/glasses.json`)).json()
     const res = orgy[Math.floor(Math.random() * orgy.length)];
     conn.sendFile(m.chat, res, 'foto.jpg', text2, m);
   }
 
   if (command == 'hentai' ) { 
-    const orgy = JSON.parse(fs.readFileSync('./json/hentai.json'));
+    const orgy = await (await fetch(`https://raw.githubusercontent.com/ketchupmaze/AssistenYulaDB/main/nsfw/hentai.json`)).json()
     const res = orgy[Math.floor(Math.random() * orgy.length)];
     conn.sendFile(m.chat, res, 'foto.jpg', text2, m);
   }
 
   if (command == 'jahy' ) { 
-    const orgy = JSON.parse(fs.readFileSync('./json/jahy.json'));
+    const orgy = await (await fetch(`https://raw.githubusercontent.com/ketchupmaze/AssistenYulaDB/main/nsfw/jahy.json`)).json()
     const res = orgy[Math.floor(Math.random() * orgy.length)];
     conn.sendFile(m.chat, res, 'foto.jpg', text2, m);
   }
 
   if (command == 'manga' ) { 
-    const orgy = JSON.parse(fs.readFileSync('./json/manga.json'));
+    const orgy = await (await fetch(`https://raw.githubusercontent.com/ketchupmaze/AssistenYulaDB/main/nsfw/manga.json`)).json()
     const res = orgy[Math.floor(Math.random() * orgy.length)];
     conn.sendFile(m.chat, res, 'foto.jpg', text2, m);
   }
 
-  if (command == 'mstb' ) { 
-    const orgy = JSON.parse(fs.readFileSync('./json/masturbation.json'));
+  if (command == 'masturbation' ) { 
+    const orgy = await (await fetch(`https://raw.githubusercontent.com/ketchupmaze/AssistenYulaDB/main/nsfw/masturbation.json`)).json()
     const res = orgy[Math.floor(Math.random() * orgy.length)];
     conn.sendFile(m.chat, res, 'foto.jpg', text2, m);
   }
 
   if (command == 'neko' ) { 
-    const orgy = JSON.parse(fs.readFileSync('./json/neko.json'));
+    const orgy = await (await fetch(`https://raw.githubusercontent.com/ketchupmaze/AssistenYulaDB/main/nsfw/neko.json`)).json()
     const res = orgy[Math.floor(Math.random() * orgy.length)];
     conn.sendFile(m.chat, res, 'foto.jpg', text2, m);
   }
 
   if (command == 'orgy' ) { 
-    const orgy = JSON.parse(fs.readFileSync('./json/orgy.json'));
+    const orgy = await (await fetch(`https://raw.githubusercontent.com/ketchupmaze/AssistenYulaDB/main/nsfw/orgy.json`)).json()
     const res = orgy[Math.floor(Math.random() * orgy.length)];
     conn.sendFile(m.chat, res, 'foto.jpg', text2, m);
   }
 
   if (command == 'panties' ) { 
-    const orgy = JSON.parse(fs.readFileSync('./json/panties.json'));
+    const orgy = await (await fetch(`https://raw.githubusercontent.com/ketchupmaze/AssistenYulaDB/main/nsfw/panties.json`)).json()
     const res = orgy[Math.floor(Math.random() * orgy.length)];
     conn.sendFile(m.chat, res, 'foto.jpg', text2, m);
   }
 
   if (command == 'pussy' ) { 
-    const orgy = JSON.parse(fs.readFileSync('./json/pussy.json'));
+    const orgy = await (await fetch(`https://raw.githubusercontent.com/ketchupmaze/AssistenYulaDB/main/nsfw/pussy.json`)).json()
     const res = orgy[Math.floor(Math.random() * orgy.length)];
     conn.sendFile(m.chat, res, 'foto.jpg', text2, m);
   }
 
   if (command == 'tentacles' ) { 
-    const orgy = JSON.parse(fs.readFileSync('./json/tentacles.json'));
+    const orgy = await (await fetch(`https://raw.githubusercontent.com/ketchupmaze/AssistenYulaDB/main/nsfw/tentacles.json`)).json()
     const res = orgy[Math.floor(Math.random() * orgy.length)];
     conn.sendFile(m.chat, res, 'foto.jpg', text2, m);
   }
 
   if (command == 'thighs' ) { 
-    const orgy = JSON.parse(fs.readFileSync('./json/thighs.json'));
+    const orgy = await (await fetch(`https://raw.githubusercontent.com/ketchupmaze/AssistenYulaDB/main/nsfw/thighs.json`)).json()
     const res = orgy[Math.floor(Math.random() * orgy.length)];
     conn.sendFile(m.chat, res, 'foto.jpg', text2, m);
   }
 
   if (command == 'cosplaynsfw' ) { 
-    const orgy = JSON.parse(fs.readFileSync('./json/cosplaynsfw.json'));
+    const orgy = await (await fetch(`https://raw.githubusercontent.com/ketchupmaze/AssistenYulaDB/main/nsfw/cosplaynsfw.json`)).json()
     const res = orgy[Math.floor(Math.random() * orgy.length)];
     conn.sendFile(m.chat, res, 'foto.jpg', text2, m);
   }
   
-  if (command == 'maid' ) { 
-    const orgy = await fetch(`https://api.zahwazein.xyz/randomanime/maid?apikey=${global.zein}`);
-    const res = await orgy.buffer()
+  if (command == 'zettai' ) { 
+    const orgy = await (await fetch(`https://raw.githubusercontent.com/ketchupmaze/AssistenYulaDB/main/nsfw/zettai.json`)).json()
+    const res = orgy[Math.floor(Math.random() * orgy.length)];
     conn.sendFile(m.chat, res, 'foto.jpg', text2, m);
   }
 }
@@ -157,6 +156,7 @@ handler.command = handler.help = [
 'blowjob', 
 'cuckold', 
 'cum', 
+'eba', 
 'ero', 
 'femdom', 
 'foot', 
@@ -172,10 +172,10 @@ handler.command = handler.help = [
 'pussy', 
 'tentacles', 
 'thighs', 
-'maid',
+'zettai',
 'cosplaynsfw'];
 handler.tags = ['nsfw'];
-handler.premium = true;
+handler.premium = false;
 handler.group = true;
 
 module.exports = handler;
